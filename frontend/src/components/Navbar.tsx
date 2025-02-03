47ac11bf04d9868d3d820ed576f52ce442a928d8
Md shehab Uddin <85302877+mdshehabsk@users.noreply.github.com>
2025-02-03 11:36:44 +0600

    implement question load UI

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 
 import { LOGO } from "../utils/logo"
-
+import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <>
@@ -8,11 +8,9 @@ const Navbar = () => {
         <div className="container">
             <div className="flex justify-between items-center">
                 <div>
-                    <a href="#">Home</a>
+                    <Link to="/">Home</Link>
                 </div>
-                <div className="hidden md:block"  >
-                    <h1 className="text-green-500 text-bold text-2xl " >নিশাত কোরিয়ান ভাষা  <span className="text-red-500" >কোচিং সেন্টার</span> </h1>
-                </div>
+
                 <div className="flex gap-2 items-center" >
                     <img className="max-h-[40px] max-w-[40px]" src={LOGO.callIcon} alt="" />
                     <p>01612212699</p>
--- frontend/src/pages/Exam/index.tsx
+++ frontend/src/pages/Exam/index.tsx
@@ -1,15 +1,65 @@
 import { useGetAllSetQuery } from "@toolkit/Exam/setApi";
-import { IMAGE } from "@utils/image";
+
 import { useLazyGetSingleSetQuery } from "@toolkit/Exam/setApi";
 
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "@toolkit/hook";
 import { handleCurrentCachedSet } from "@toolkit/Exam/setSlice";
+import { useRef, useState } from "react";
+import React from "react";
 const Exam = () => {
+  const [isTakeExamModal, setIsTakeExamModal] = useState(false);
+  const progressRef = useRef<HTMLHeadingElement>(null);
+  const totalSizeRef = useRef<HTMLSpanElement>(null);
+  const loadedSizeRef = useRef<HTMLSpanElement>(null);
   const navigate = useNavigate();
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   const { data: allSet } = useGetAllSetQuery(undefined);
   const [trigger] = useLazyGetSingleSetQuery();
+
+  const progress = { totalSize: 0, downloadedSize: 0 };
+
+  async function fetchWithProgress(url: string): Promise<string> {
+    const response = await fetch(url);
+    const contentLength = response.headers.get("content-length");
+
+    if (contentLength) {
+      progress.totalSize += parseInt(contentLength, 10);
+    }
+
+    const reader = response.body?.getReader();
+    const chunks: Uint8Array[] = [];
+    while (reader) {
+      const { done, value } = await reader.read();
+      if (done) break;
+
+      chunks.push(value);
+
+      progress.downloadedSize += value.length;
+
+      if (progressRef.current) {
+        progressRef.current.style.width = ` ${(
+          (progress.downloadedSize / progress.totalSize) *
+          100
+        ).toFixed(2)}%  `;
+      }
+      if (loadedSizeRef.current) {
+        loadedSizeRef.current.innerText = `${(
+          progress.downloadedSize /
+          (1024 * 1024)
+        ).toFixed(2)} MB`;
+      }
+      if (totalSizeRef.current) {
+        totalSizeRef.current.innerText = `${(
+          progress.totalSize /
+          (1024 * 1024)
+        ).toFixed(2)} MB`;
+      }
+    }
+
+    const blob = new Blob(chunks);
+    return URL.createObjectURL(blob);
+  }
 
   async function handleTakeExam(id: string) {
     const { data } = await trigger(id).unwrap();
@@ -19,24 +69,21 @@ const Exam = () => {
       text: "text",
     };
 
-  const updatedData =  await Promise.all(
+    const updatedData = await Promise.all(
       data?.questions?.map(async (item) => {
         // Fetch description if it's a file
         let newDescription: string | Blob = item.description;
         if (mappedTyped[item.descriptionType] === "file") {
-          const response = await fetch(item.description);
-          newDescription = URL.createObjectURL(await response.blob());
+          newDescription = await fetchWithProgress(item.description);
         }
 
         // Fetch all options if optionsType is "file"
         let newOptions: string[] | Blob[] = item.options;
         if (mappedTyped[item.optionsType] === "file") {
           newOptions = await Promise.all(
-            item.options.map(async (option) => {
-              const response = await fetch(option);
-              const fileBlob = URL.createObjectURL(await response.blob());
-              return fileBlob;
-            })
+            (newOptions = await Promise.all(
+              item.options.map(fetchWithProgress)
+            ))
           );
         }
 
@@ -48,21 +95,14 @@ const Exam = () => {
       ...data,
       questions: updatedData,
     };
-    dispatch(handleCurrentCachedSet(newSet))
+    dispatch(handleCurrentCachedSet(newSet));
     navigate(`/set/prefetch`);
   }
 
   return (
-    <>
+    <React.Fragment>
       <div className="flex flex-wrap min-h-lvh w-full  ">
-        <div className="basis-5/12 hidden md:block ">
-          <img
-            className="h-full w-full object-cover"
-            src={IMAGE.koreanExam}
-            alt="image description"
-          />
-        </div>
-        <div className="basis-full md:basis-6/12  p-8 flex flex-wrap gap-2 grow ">
+        <div className="basis-full md:basis-6/12 lg:basis-4/12  p-8 flex flex-wrap gap-2  ">
           {allSet?.data?.map((elem, index) => (
             <div
               key={index}
@@ -74,25 +114,52 @@ const Exam = () => {
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {elem?.description}
               </p>
-              {/* <img src={URL.createObjectURL(blob?.[0]?.description)} alt="" /> */}
-              {/* <Link
-                to={`/set/${elem._id}`}
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:scale-105 transition-all focus:outline-none "
-                state={{name:'md shehab'}}
-              >
-                Take Exam
-              </Link> */}
               <button
-                onClick={() => handleTakeExam(elem?._id)}
+                onClick={() => setIsTakeExamModal(true)}
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:scale-105 transition-all focus:outline-none "
               >
                 Take Exam
               </button>
+              {isTakeExamModal && (
+                <div className="w-screen h-screen bg-black/35 fixed top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+                  <div className=" py-20 w-full md:w-[80%] lg:w-[50%] mx-auto shadow-md fixed top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 bg-slate-50">
+                    <h2 className="text-center font-bold text-2xl" > {elem.name} </h2>
+                    <div className="px-5">
+                      <div className="flex justify-between">
+                        <span ref={loadedSizeRef}></span>
+                        <span ref={totalSizeRef}></span>
+                      </div>
+                      <div className="flex w-full h-[20px] ">
+                        <div
+                          className=" h-full  bg-green-600  text-sm text-white text-end "
+                          ref={progressRef}
+                        >
+                          {" "}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-center gap-5 my-2 h-full">
+                      <button
+                        className="px-8 w-[200px] py-2 bg-green-600 text-white rounded-full text-2xl "
+                        onClick={() => handleTakeExam(elem._id)}
+                      >
+                        Start Exam
+                      </button>
+                      <button
+                        className="px-8 w-[200px] py-2 bg-red-600 text-white rounded-full text-2xl"
+                        onClick={() => setIsTakeExamModal(false)}
+                      >
+                        Exit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           ))}
         </div>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
